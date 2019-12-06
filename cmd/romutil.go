0eec173c29c848929f8608227252911a9da72ab9
Zorchenhimer <zorchenhimer@gmail.com>
2019-12-05 21:05:11 -0500

    Remove pack_rom and unpack_rom
    These two utilities are being rolled into romutil.  The functionality
    isn't tested yet, but the foundation is there.

--- cmd/romutil.go
+++ cmd/romutil.go
@@ -25,7 +25,9 @@ func main() {
 	case "unpack":
 		cmdUnpack(os.Args[2])
 	case "pack":
-		cmdPack(os.Args[2])
+		// TODO: test this and clean it up
+		dir := strings.Trim(os.Args[2], `/\`) + "/"
+		cmdPack(dir, "packed.nes")
 	case "info":
 		cmdInfo(os.Args[2])
 	case "usage":
@@ -37,9 +39,49 @@ func main() {
 	}
 }
 
-func cmdPack(dirName string) {
-	// TODO
-	fmt.Printf("TODO: Pack directory %q\n", dirName)
+func cmdPack(dirName, romName string) {
+	headerRaw, err := ioutil.ReadFile(dirName + "header.json")
+	if err != nil {
+		fmt.Printf("Unable to read header.json file: %v\n", err)
+		os.Exit(1)
+	}
+
+	header, err := ines.LoadHeader(headerRaw)
+	if err != nil {
+		fmt.Printf("Unable to load header data: %v\n", err)
+		os.Exit(1)
+	}
+
+	prgRaw, err := ioutil.ReadFile(dirName + "prg.dat")
+	if err != nil {
+		fmt.Printf("Unable to read prg.dat file: %v\n", err)
+		os.Exit(1)
+	}
+
+	chr := []byte{}
+	for i := 0; i < 16; i++ {
+		chrfile := fmt.Sprintf("bank_%02d.chr", i)
+		fmt.Println(chrfile)
+		chrRaw, err := ioutil.ReadFile(dirName + chrfile)
+		if err != nil {
+			fmt.Printf("Unable to read %s file: %v\n", chrfile, err)
+			os.Exit(1)
+		}
+
+		chr = append(chr, chrRaw...)
+	}
+
+	rom := ines.NesRom{
+		Header: header,
+		PrgRom: prgRaw,
+		ChrRom: chr,
+	}
+
+	err = rom.WriteFile(romName)
+	if err != nil {
+		fmt.Printf("Unable to write rom: %v\n", err)
+		os.Exit(1)
+	}
 }
 
 func cmdUnpack(filename string) {
@@ -84,6 +126,9 @@ func cmdUnpack(filename string) {
 			os.Exit(1)
 		}
 	}
+
+	// unpack_rom originally split the CHR into 8k chunks.  Should this
+	// be an option here?
 }
 
 func cmdInfo(filename string) {
