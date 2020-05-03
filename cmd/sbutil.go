0a2efe872ac428bab2e67182a76474f64f77a1b6
Zorchenhimer <zorchenhimer@gmail.com>
2020-05-03 03:36:37 -0400

    Add some packet types; dump stuff to files
    - Added a bunch of packet types (bulk data, mark start/end...).
    - Dump "asm" and CHR data to files.
    - Process all pages until an error is encountered.
    - Other fixes.

--- cmd/sbutil.go
+++ cmd/sbutil.go
@@ -8,7 +8,7 @@ import (
 	"github.com/zorchenhimer/go-nes/studybox"
 )
 
-const inputDir string = `F:/nes/StudyBox/studybox-emu/StudyBoxOthers/`
+const inputDir string = `` //`F:/nes/StudyBox/studybox-emu/StudyBoxOthers/`
 const inputFile string = `ニュートンランド小3 算数 04月号 (13CE04).studybox`
 
 func main() {
@@ -31,10 +31,39 @@ func main() {
 		fmt.Println(page)
 	}
 
-	decoded, err := studybox.DecodePage(sb.Data.Pages[0])
-	if err != nil {
-		fmt.Println(err)
-	}
+	for pidx, page := range sb.Data.Pages {
+		decoded, err := studybox.DecodePage(page)
+		if err != nil {
+			fmt.Printf("==> %v\n", err)
+		}
 
-	fmt.Println(decoded)
+		file, err := os.Create(fmt.Sprintf("Page_%02d.txt", pidx))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Fprintln(file, decoded)
+		file.Close()
+
+		var data bool
+		chrData := []byte{}
+		for _, packet := range decoded.Packets {
+			meta := packet.Meta()
+			if meta.State == 2 && meta.Type == 4 {
+				data = true
+			} else if bulk, ok := packet.(*studybox.PacketBulkData); data && ok {
+				chrData = append(chrData, bulk.Data...)
+			} else if meta.State == 1 && meta.Type == 0 {
+				data = false
+			}
+		}
+
+		if len(chrData) > 0 {
+			err = ioutil.WriteFile(fmt.Sprintf("chrData_page%02d.chr", pidx), chrData, 0777)
+			if err != nil {
+				fmt.Println("Unable to write data to file: ", err)
+				os.Exit(1)
+			}
+		}
+	}
 }
