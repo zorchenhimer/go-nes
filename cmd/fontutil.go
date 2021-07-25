b542a14c92974434e7d0051935447f3e36bcde4f
Zorchenhimer <zorchenhimer@gmail.com>
2021-07-25 12:33:55 -0400

    Refactor a bit of fontutil
    - Swapped out flag in the standard lib with go-arg to simplify argument
      parsing.  This gives all the short options long versions and better
      --help output.
    - Make the Widths and Remap file outputs optional.
    - Change the main output's file permissions to 0644.
    - Add the character in a comment to each line of output in the Remap and
      Widths files.
    - Make the width of a space configurable with the --space-width option.
    - Add option to start processing the input at a tile offset with
      --input-offset.
    - Add option to limit the number of tiles processed with the
      --input-length option.  A value of zero disables this.

--- cmd/fontutil.go
+++ cmd/fontutil.go
@@ -1,91 +1,102 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
+
+	"github.com/alexflint/go-arg"
 
 	nesimg "github.com/zorchenhimer/go-nes/image"
 )
 
-var (
-	opt_Output string // output CHR asm file
-	opt_Remap  string // output .charmap remapping file
-	opt_Width  string // output width listing file
-	opt_Input  string // input bmp file
-)
+type options struct {
+	// Input BMP file
+	Input string `arg:"-i,--input,required" help:"Input bitmap file containing the font."`
+
+	// Output files
+	Output string `arg:"-o,--output,required" help:"Output file for CHR ASM data."`
+	Remap  string `arg:"-r,--remap" help:"Output file for ca65 character remappings."`
+	Widths string `arg:"-w,--widths" help:"Output file for character width values."`
+
+	//Help bool `arg:"-h,--help" help:"Print help and exit."`
+
+	//Verbose bool `arg:"-v,--verbose" help:"Add verbosity"`
+	InputOffset int `arg:"--input-offset" default:"0" help:"Tile offset to start reading at from the input file."`
+	InputLength int `arg:"--input-length" default:"0" help:"Number of tiles to look at.  Zero turns this off."`
+
+	SpaceWidth int `arg:"-s,--space-width" default:"5" help:"Width of the SPACE character."`
+}
 
 // Take input image file (single file) and output the tile-reduced
 // font CHR (as assembly), width values, and character remappings
 // to three files.
 func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "A simple command line utility to take a single bitmap of a font and convert it into a single-plane, tile-reduced, CHR in assembly with a chracter remap file and a list of character widths.\n\nRequired options:\n")
-		flag.PrintDefaults()
-	}
+	var opts *options = &options{}
+	parser := arg.MustParse(opts)
 
-	flag.StringVar(&opt_Output, "o", "", "Output CHR asm file")
-	flag.StringVar(&opt_Remap, "r", "", "Output .charmap remapping file file")
-	flag.StringVar(&opt_Width, "w", "", "Output widths file")
-	flag.StringVar(&opt_Input, "i", "", "Input BMP file")
-	flag.Parse()
-
-	opt_Output = strings.TrimSpace(opt_Output)
-	opt_Remap = strings.TrimSpace(opt_Remap)
-	opt_Input = strings.TrimSpace(opt_Input)
-
-	if opt_Output == "" {
+	if opts.Output == "" {
 		fmt.Println("Missing output file\n")
-		flag.Usage()
+		parser.WriteUsage(os.Stdout)
 		os.Exit(1)
 	}
 
-	if opt_Remap == "" {
-		fmt.Println("Missing remap file\n")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	if opt_Width == "" {
-		fmt.Println("Missing width file\n")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	if opt_Input == "" {
+	if opts.Input == "" {
 		fmt.Println("Missing input file\n")
-		flag.Usage()
+		parser.WriteUsage(os.Stdout)
 		os.Exit(1)
 	}
 
-	if filepath.Ext(opt_Input) != ".bmp" {
+	if filepath.Ext(opts.Input) != ".bmp" {
 		fmt.Println("Only bitmap images are supported as input\n")
-		flag.Usage()
+		parser.WriteUsage(os.Stdout)
 		os.Exit(1)
 	}
 
-	pt, err := nesimg.LoadBitmap(opt_Input)
+	if opts.InputOffset < 0 {
+		fmt.Println("Input offset cannot be negative")
+		os.Exit(1)
+	}
+
+	pt, err := nesimg.LoadBitmap(opts.Input)
 	if err != nil {
 		fmt.Printf("Unable to load bitmap: %v\n", err)
 		os.Exit(1)
 	}
 
-	pt.RemoveDuplicates(true)
-	if err = writeRemap(opt_Remap, pt); err != nil {
-		fmt.Printf("Unable to write remap file: %v\n", err)
+	if opts.InputOffset >= len(pt.Patterns) {
+		fmt.Println("Input offset is larger that the tile count")
 		os.Exit(1)
 	}
 
-	if err = writeWidths(opt_Width, pt); err != nil {
-		fmt.Printf("Unable to write widths file: %v\n", err)
-		os.Exit(1)
+	if opts.InputOffset > 0 {
+		if opts.InputLength == 0 {
+			opts.InputLength = len(pt.Patterns)
+		}
+		pt.Patterns = pt.Patterns[opts.InputOffset:opts.InputLength]
+	}
+
+	// Remove dupes manually after the input offset so the first tile
+	// in the output has the correct OrigId value.
+	pt.RemoveDuplicates(false)
+
+	if opts.Remap != "" {
+		if err = writeRemap(opts.Remap, pt); err != nil {
+			fmt.Printf("Unable to write remap file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	if opts.Widths != "" {
+		if err = writeWidths(opts.Widths, pt, opts.SpaceWidth); err != nil {
+			fmt.Printf("Unable to write widths file: %v\n", err)
+			os.Exit(1)
+		}
 	}
 
 	data := []byte(pt.Asm(true))
-	if err = ioutil.WriteFile(opt_Output, data, 0777); err != nil {
+	if err = ioutil.WriteFile(opts.Output, data, 0644); err != nil {
 		fmt.Printf("Unable to write ASM file: %v\n", err)
 		os.Exit(1)
 	}
@@ -98,19 +109,36 @@ func writeRemap(filename string, pt *nesimg.PatternTable) error {
 	}
 
 	for idx, tile := range pt.Patterns {
-		fmt.Fprintf(file, ".charmap $%02X, $%02X\n", tile.OrigId, idx)
+		// Use hex notation for non-printing characters
+		if tile.OrigId < 0x20 || tile.OrigId > 0x7E {
+			fmt.Fprintf(file, ".charmap $%02X, $%02X ; $%X\n", tile.OrigId, idx, tile.OrigId)
+		} else {
+			fmt.Fprintf(file, ".charmap $%02X, $%02X ; '%c'\n", tile.OrigId, idx, tile.OrigId)
+		}
 	}
 	return file.Close()
 }
 
-func writeWidths(filename string, pt *nesimg.PatternTable) error {
+func writeWidths(filename string, pt *nesimg.PatternTable, spaceWidth int) error {
 	file, err := os.Create(filename)
 	if err != nil {
 		return fmt.Errorf("Unable to create widths file: %v", err)
 	}
 
 	for _, tile := range pt.Patterns {
-		fmt.Fprintf(file, ".byte %d\n", tile.CharacterWidth()+1)
+		w := tile.CharacterWidth()
+		if w == 0 {
+			w = spaceWidth
+		} else {
+			w += 1
+		}
+
+		// Use hex notation for non-printing characters
+		if tile.OrigId < 0x20 || tile.OrigId > 0x7E {
+			fmt.Fprintf(file, ".byte %d ; $%X\n", w, tile.OrigId)
+		} else {
+			fmt.Fprintf(file, ".byte %d ; '%c'\n", w, tile.OrigId)
+		}
 	}
 	return file.Close()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zorchenhimer/go-nes
 
 go 1.12
 
-require github.com/mitchellh/go-wordwrap v1.0.0
+require (
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0
+)
